b970b36b07ebcde58478323398fdd91c204cf7ad
Wout <wout.mergaerts@gmail.com>
2020-06-03 11:57:43 +0200

    Update circleci & prop dependency

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Video = (props) => {
+    const [ready, setReady] = useState(false);
+
     useEffect(() => {
         let video = document.getElementById("video");
 
@@ -12,15 +14,20 @@ const Video = (props) => {
                     //video.src = window.URL.createObjectURL(stream);
                     video.srcObject = stream;
                     video.play();
-                    props.ready()
+                    setReady(true)
                 })
                 .catch((error) => {
                     window.alert(
-                        "Please check your permissions: access to camera is needed to estimate head direction to control the snake."
+                        "Please check your permissions: access to the camera is needed to estimate head position to control the snake."
                     );
                 });
         }
     }, []);
+
+
+    useEffect(() => {
+        if (ready) props.ready()
+    }, [props, ready])
 
     return (
         <video id="video" height="240" autoPlay playsInline></video>
